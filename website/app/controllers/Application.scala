4dc50f2f36b4c0ec37fdc04da7bf6616b3e8bd9f
Christophe Bram <cbramdit@gmail.com>
2014-10-03 20:08:09 +0200

    End of session at the library

--- website/app/controllers/Application.scala
+++ website/app/controllers/Application.scala
@@ -7,6 +7,14 @@ object Application extends Controller {
   val doNotCachePage = Array(CACHE_CONTROL -> "no-cache, no-store")
 
   def index = Action { request =>
-      Ok(views.html.index())
+    Ok(views.html.index())
+  }
+
+  def projects = Action { request =>
+    Redirect("/", 301)
+  }
+
+  def appearShowroom = Action { request =>
+    Ok(views.html.appearShowroom())
   }
 }
